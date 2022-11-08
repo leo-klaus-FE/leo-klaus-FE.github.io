ed21052068f71984cfe4a47dfe27b64a4f31f23c
Leo C <leoacoleman@gmail.com>
2022-11-08 20:50:26 +0200

    fix(browser): Correcting browser warnings

--- src/component/Checkbox.tsx
+++ src/component/Checkbox.tsx
@@ -1,8 +1,4 @@
-import React, { useState } from 'react';
-
-const handleChange = () => {
-    console.log('blah')
-}
+import React from 'react';
 
 export const Checkbox = ({ selected, id }) => {
     return <input type="radio" name={id} id={id} checked={selected} onClick={() => { }} />
